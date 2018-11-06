f8e72a774758d18729494113b1034c6e8cd2fa4a
danialrajabi <rajabi.d.b@gmail.com>
2018-11-06 22:26:28 +0330

    - Update Login template - add static files to user app - Update urls.py(main)

--- jadiblog/jadiblog/urls.py
+++ jadiblog/jadiblog/urls.py
@@ -25,4 +25,4 @@ urlpatterns = [
                 path('', include('blog.urls')),
                 path('users/', include('users.urls')),
                 path('admin/', admin.site.urls),
-              ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
