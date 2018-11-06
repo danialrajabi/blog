69d9d6003da9c0b6728ac3bcf721edad448f914a
danialrajabi <rajabi.d.b@gmail.com>
2018-11-06 13:00:41 +0330

    - add blogpageListview in views.py - add blogpage url in urls.py - update navbar in header.html

--- jadiblog/blog/urls.py
+++ jadiblog/blog/urls.py
@@ -5,6 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
+    path('blog', views.BlogListView.as_view(), name='blogpage'),
     re_path('^post/(?P<slug>[^/]+)/?$', views.PostDetail.as_view(), name='post_detail'),
 
 ]
--- jadiblog/blog/views.py
+++ jadiblog/blog/views.py
@@ -20,6 +20,22 @@ class HomeView(generic.ListView):
         return context
 
 
+class BlogListView(generic.ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = '-created_at'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(category__name='Blog')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recent_post'] = Post.objects.all().order_by('-created_at')[:NUMBER_RECENT_POSTS]
+        return context
+
+
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
