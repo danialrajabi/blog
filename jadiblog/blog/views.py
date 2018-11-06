d1e79c0be534478bf2e60e49692d57dd345fc782
danialrajabi <rajabi.d.b@gmail.com>
2018-11-06 12:47:11 +0330

    - Update Views: add recent posts to content data - Update recent posts in sidebar.html

--- jadiblog/blog/views.py
+++ jadiblog/blog/views.py
@@ -5,6 +5,8 @@ from . models import Post
 
 # Create your views here.
 
+NUMBER_RECENT_POSTS = 3
+
 
 class HomeView(generic.ListView):
     model = Post
@@ -12,8 +14,18 @@ class HomeView(generic.ListView):
     context_object_name = 'posts'
     ordering = '-created_at'
 
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recent_post'] = Post.objects.all().order_by('-created_at')[:NUMBER_RECENT_POSTS]
+        return context
+
 
 class PostDetail(generic.DetailView):
     model = Post
     template_name = 'blog/post_detail.html'
     context_object_name = 'post'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['recent_post'] = Post.objects.all().order_by('-created_at')[:NUMBER_RECENT_POSTS]
+        return context
